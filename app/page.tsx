b7c70fa76be663845241cb93d4f8442410a70b2f
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-08-31 16:57:39 -0700

    Added daisy ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-12">
@@ -6,8 +8,21 @@ export default function Home() {
         <p>about</p>
       </header>
       <div>
-        <h1>midpoint!</h1>
+        <h1 className="text-3xl">midpoint</h1>
       </div>
+      <div id="firstLocation">
+        <div className="flex flex-row mt-10">
+          <input type="text" placeholder="First location zip code" className="input input-bordered w-full max-w-xs m-3" />
+          <button className="btn m-3">Enter</button>
+        </div>
+      </div>
+      <div id="secondLocation">
+        <div className="flex flex-row mt-5">
+          <input type="text" placeholder="Second location zip code" className="input input-bordered w-full max-w-xs m-3" />
+          <button className="btn m-3">Enter</button>
+        </div>
+      </div>
+      <button className="btn btn-lg m-10">Find Midpoint</button>
     </main>
   )
 }
