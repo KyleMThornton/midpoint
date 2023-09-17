ed03a05a4eb366ae94dfcbed8dca1aec361bbb83
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-09-17 14:52:09 -0700

    Add rapidapi route, basic functionality working

---
+++ app/api/rapidapi/route.tsx
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(req:NextRequest, res:NextResponse) {
+    const RAPID_API_KEY = process.env.YELP_API_KEY;
+    const lat = req.nextUrl.searchParams.get('lat');
+    const lon = req.nextUrl.searchParams.get('lon');
+
+    try {
+        const response = await fetch(`https://wft-geo-db.p.rapidapi.com/v1/geo/cities?location=${lat}${lon}&limit=1`, {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '8e96c91d02mshd89e9e3724edd2dp16cea9jsn9eb5c9f74fa0',
+            'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+          }
+        })
+        const data = await response.json();
+        return NextResponse.json(data);
+    } catch(error) {
+        return NextResponse.json({status: 500});
+    }
+}
