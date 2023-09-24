b9daef7c70a83b1c8b4a8966202e89c377d1c0a9
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-09-24 11:41:24 -0700

    Route updates

--- app/api/rapidapi/route.tsx
+++ app/api/rapidapi/route.tsx
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(req:NextRequest, res:NextResponse) {
-    const RAPID_API_KEY = process.env.YELP_API_KEY;
+    const RAPID_API_KEY = process.env.RAPID_API_KEY;
     const lat = req.nextUrl.searchParams.get('lat');
     const lon = req.nextUrl.searchParams.get('lon');
 
@@ -9,7 +9,7 @@ export async function GET(req:NextRequest, res:NextResponse) {
         const response = await fetch(`https://wft-geo-db.p.rapidapi.com/v1/geo/cities?location=${lat}${lon}&minPopulation=5000&limit=1`, {
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': '8e96c91d02mshd89e9e3724edd2dp16cea9jsn9eb5c9f74fa0',
+            'X-RapidAPI-Key': `${RAPID_API_KEY}`,
             'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
           }
         })
