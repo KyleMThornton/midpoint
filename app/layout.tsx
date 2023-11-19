852025c6318305899337be3c0f45670f0ce39f3a
Kyle Thornton <kylemthornton@gmail.com>
2023-11-18 17:00:02 -0800

    Dark mode fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,11 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <Providers> */}
+        <Providers>
           {children}
           <Footer />
           <Analytics />
-        {/* </Providers> */}
+        </Providers>
       </body>
     </html>
   )
