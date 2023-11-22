3ff508dd9df76db2be01aece50b769c1004539d9
Kyle Thornton <kylemthornton@gmail.com>
2023-11-22 12:49:54 -0800

    DaisyUI theme fixes

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+        <footer className="footer footer-center p-4 bg-base-200 text-base-content">
             <aside>
             <p>Made with ❤️ by <a href="https://www.kylethornton.dev" target="_blank">Kyle Thornton</a></p>
             </aside>
